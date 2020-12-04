cd45cb6c1905899fa81028ed92283858809dfeb4
layyne <schnubnugget@gmail.com>
2020-12-04 11:37:45 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use day4::Config;
+use std::env;
+use std::process;
+use std::result::Result;
+
+fn main() -> Result<(), ()> {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Argument parse error: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = day4::run(config) {
+        println!("run() error: {}", e);
+        process::exit(1);
+    };
+
+    Ok(())
+}
